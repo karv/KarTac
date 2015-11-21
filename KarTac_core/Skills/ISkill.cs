9aa2ca54e67c265fccb540237f08017d39760172
Edgar Carballo <karvayoEdgar@gmail.com>
2015-11-21 16:51:28 -0600

    ISkill debe poder verificar si es posible usarse

--- KarTac_core/Skills/ISkill.cs
+++ KarTac_core/Skills/ISkill.cs
@@ -7,7 +7,13 @@ namespace KarTac.Skills
 	{
 		string Nombre { get; }
 
-		void Ejecutar (Campo campo);
+		void Ejecutar (Unidad usuario, Campo campo);
+
+		/// <summary>
+		/// Determina si es posible usar este skill.
+		/// </summary>
+		/// <param name="campo">Campo.</param>
+		/// <param name="usuario">Usuario</param>
+		bool Usable (Unidad usuario, Campo campo);
 	}
 }
-
