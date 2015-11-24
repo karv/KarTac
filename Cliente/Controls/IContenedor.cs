80a35749ae655145a07141ee042b21868aa55a86
Edgar Carballo <karvayoEdgar@gmail.com>
2015-11-23 23:37:40 -0600

    + IContenedor Contenedor de IControl

---
+++ Cliente/Controls/IContenedor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace KarTac.Cliente.Controls
+{
+	/// <summary>
+	/// Contenedor de controles
+	/// </summary>
+	public interface IContenedor : IControl
+	{
+		ICollection<IControl> Controles { get; }
+	}
+}
+
