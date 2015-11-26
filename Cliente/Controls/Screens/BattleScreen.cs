0a87fa9fa6752ac6821b8c351fca97e61590ecff
Edgar Carballo <karvayoEdgar@gmail.com>
2015-11-25 23:10:17 -0600

    + BattleScreen

---
+++ Cliente/Controls/Screens/BattleScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace KarTac.Cliente.Controls.Screens
+{
+	public class BattleScreen: Screen
+	{
+		public List<Unidad> Unidades { get; }
+
+		public BattleScreen (KarTacGame juego)
+			: base (juego)
+		{
+		}
+
+		/// <summary>
+		/// Agrega una unidad
+		/// </summary>
+		/// <returns>Devuelve el control (recién creado) asociado a la unidad</returns>
+		public Unidad AgregaUnidad (KarTac.Batalla.Unidad unit)
+		{
+			var ret = new Unidad (this, unit);
+			ret.Initialize ();
+			ret.Include ();
+			return ret;
+		}
+	}
+}
+
