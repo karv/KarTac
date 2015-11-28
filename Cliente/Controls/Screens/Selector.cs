a96e77553977736e55dd5a0e1514b53958aecc92
Edgar Carballo <karvayoEdgar@gmail.com>
2015-11-27 18:38:13 -0600

    + Selector : ISelectorTarget

---
+++ Cliente/Controls/Screens/Selector.cs
@@ -0,0 +1,39 @@
+﻿using KarTac.Batalla;
+
+namespace KarTac.Cliente.Controls.Screens
+{
+	public class Selector: ISelectorTarget
+	{
+		/// <summary>
+		/// Revisa si es posible dar una salida con la función Selecciona
+		/// </summary>
+		public bool Validar ()
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Ejecuta el selector y devuelve los seleccionados
+		/// </summary>
+		public System.Collections.Generic.IList<KarTac.Batalla.Unidad> Selecciona ()
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		/// <summary>
+		/// Espablece el máximo número
+		/// </summary>
+		/// <value>The max select.</value>
+		public int MaxSelect { get; set; }
+
+		/// <summary>
+		/// Establece si el número máximo de blancos es el realidad el número exacto de blancos.
+		/// </summary>
+		public bool IgualdadEstricta { get; set; }
+
+		/// <summary>
+		/// Establece los posibles blancos
+		/// </summary>
+		public System.Collections.Generic.IList<KarTac.Batalla.Unidad> PosiblesBlancos { get; set; }
+	}
+}
