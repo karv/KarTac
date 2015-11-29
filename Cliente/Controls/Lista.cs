b5ea2913ffb353ae063880645ca5e7db7a4ff56d
Edgar Carballo <karvayoEdgar@gmail.com>
2015-11-29 16:32:58 -0600

    + Clase estática Primitivos para dibujar líneas

---
+++ Cliente/Controls/Lista.cs
@@ -0,0 +1,29 @@
+﻿using KarTac.Cliente.Controls.Screens;
+
+namespace KarTac.Cliente.Controls
+{
+	public class Lista : SBC
+	{
+		public Lista (IScreen screen)
+			: base (screen)
+		{
+		}
+
+		public override void Dibujar (Microsoft.Xna.Framework.GameTime gameTime)
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		public override Microsoft.Xna.Framework.Rectangle GetBounds ()
+		{
+			throw new System.NotImplementedException ();
+		}
+
+		public override void LoadContent ()
+		{
+			throw new System.NotImplementedException ();
+		}
+
+	}
+}
+
