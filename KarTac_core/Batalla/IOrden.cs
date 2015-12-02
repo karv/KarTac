d5e77c56d9c127e4df60789cb8c48b37383cb3e0
Edgar Carballo <karvayoEdgar@gmail.com>
2015-12-02 16:59:33 -0600

    + IOrden El usuario no puede estar dándo una órden cada ciclo, esta interface se encarga de eso

---
+++ KarTac_core/Batalla/IOrden.cs
@@ -0,0 +1,13 @@
+﻿namespace KarTac.Batalla
+{
+	/// <summary>
+	/// Da la orden de qué hacer en cada Update de Unidad,
+	/// en términos de primitivos
+	/// </summary>
+	public interface IOrden
+	{
+		Unidad Unidad { get; }
+
+		void Update ();
+	}
+}
