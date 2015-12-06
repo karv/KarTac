7cb511eca9fa49e85a7ad149ea6257fcd0ec8078
Edgar Carballo <karvayoEdgar@gmail.com>
2015-12-06 16:22:54 -0600

    Empuje.ctor

--- KarTac_core/Personajes/Empuje.cs
+++ KarTac_core/Personajes/Empuje.cs
@@ -5,5 +5,12 @@
 		public float HaciaEnemigo;
 		public float HaciaAliado;
 		public float Masa;
+
+		public Empuje (float haciaEnemigo, float haciaAliado, float masa)
+		{
+			HaciaEnemigo = haciaEnemigo;
+			HaciaAliado = haciaAliado;
+			Masa = masa;
+		}
 	}
 }
