83f8ff1b73cb8de8751a94c29a16c76881e1c76c
Edgar Carballo <karvayoEdgar@gmail.com>
2015-12-10 17:11:36 -0600

    + clase Clan

---
+++ KarTac_core/Clan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using KarTac.Personajes;
+using System;
+
+namespace KarTac
+{
+	/// <summary>
+	/// Clase principal de el estado de un juego, en forma global.
+	/// </summary>
+	public class Clan
+	{
+		/// <summary>
+		/// Personajes
+		/// </summary>
+		public List<Personaje> Personajes;
+
+		/// <summary>
+		/// Fondos del clan
+		/// </summary>
+		public int Dinero { get; set; }
+
+		public void Guardar ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Cargar ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
