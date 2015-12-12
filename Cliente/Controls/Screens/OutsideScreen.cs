3c0c7cb4e9a652dcfe1de5592a668f1ecc540277
Edgar Carballo <karvayoEdgar@gmail.com>
2015-12-12 01:50:08 -0600

    Reducir tamaño de texto (HP) en lista entre batallas

--- Cliente/Controls/Screens/OutsideScreen.cs
+++ Cliente/Controls/Screens/OutsideScreen.cs
@@ -16,7 +16,7 @@ namespace KarTac.Cliente.Controls.Screens
 			MyClan = clan;
 
 			personajes = new Lista<Personaje> (this);
-			personajes.Stringificación = x => x.Nombre + " " + x.Atributos.HP;
+			personajes.Stringificación = x => x.Nombre + " HP: " + x.Atributos.HP.Valor;
 			personajes.Bounds = new Rectangle (10, 10, 400, 300);
 			iniciar = new Botón (this, new Rectangle (500, 30, 30, 30));
 			iniciar.Textura = @"Icons/sword";
