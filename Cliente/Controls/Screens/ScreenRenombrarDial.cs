93b286d7a14d65063ba1c6abd2ed0bc3175fecf1
Edgar Carballo <karvayoEdgar@gmail.com>
2015-12-14 15:24:53 -0600

    + ScreenRenombrarDial

---
+++ Cliente/Controls/Screens/ScreenRenombrarDial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace KarTac.Cliente.Controls.Screens
+{
+	public class ScreenRenombrarDial : ScreenDial
+	{
+		readonly EntradaTexto input;
+
+		public ScreenRenombrarDial (KarTacGame juego, IScreen screen)
+			: base (juego, screen)
+		{
+			input = new EntradaTexto (screen);
+			input.Pos = new Point (100, 100);
+		}
+
+		public string Texto { get; private set; }
+
+		public override void Update (GameTime gameTime)
+		{
+			if (InputManager.FuePresionado (OpenTK.Input.Key.Enter))
+			{
+				Texto = input.Texto;
+				Salir ();
+			}
+			base.Update (gameTime);
+		}
+	}
+}
