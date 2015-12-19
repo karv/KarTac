9f2eaa2c6fd959d587f2d12b4e610aa60614d93f
Edgar Carballo <karvayoEdgar@gmail.com>
2015-12-19 16:03:38 -0600

    Agregar funciones de (des)equipar en IEquipamento

--- KarTac_core/Equipos/IEquipamento.cs
+++ KarTac_core/Equipos/IEquipamento.cs
@@ -6,11 +6,37 @@ namespace KarTac.Equipos
 {
 	public interface IEquipamento
 	{
+		/// <summary>
+		/// Personaje que porta este equipamento,
+		/// </summary>
 		Personaje Portador { get; set; }
 
+		/// <summary>
+		/// Tags
+		/// </summary>
 		IEnumerable<string> Tags { get; }
 
-		event Action AlEquipar;
-		event Action ElDesequipar;
+		/// <summary>
+		/// Se equipa en un personaje.
+		/// Se desequipa de su personaje anterior
+		/// </summary>
+		void EquiparEn (Personaje personaje);
+
+		/// <summary>
+		/// Se desequipa
+		/// </summary>
+		void Desequipar ();
+
+		/// <summary>
+		/// Ocurre al equiparse.
+		/// Primer argumento es el usuario anterior.
+		/// </summary>
+		/// 
+		event Action<Personaje> AlEquipar;
+		/// <summary>
+		/// Ocurre al desequiparse.
+		/// Argumento es sy usuario anterior.
+		/// </summary>
+		event Action<Personaje> ElDesequipar;
 	}
 }
