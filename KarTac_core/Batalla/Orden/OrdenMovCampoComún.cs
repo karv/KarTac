d44f79fc3e3f08712dc99778b4cd81ab98ca8c3c
Edgar Carballo <karvayoEdgar@gmail.com>
2016-01-04 14:46:42 -0600

    Hacer clase base para movimiento por campos vectoriales

---
+++ KarTac_core/Batalla/Orden/OrdenMovCampoComún.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace KarTac.Batalla.Orden
+{
+	public abstract class OrdenMovCampoComún : IOrden
+	{
+		public OrdenMovCampoComún (Unidad unidad)
+		{
+			Unidad = unidad;
+		}
+
+		public Unidad Unidad { get; }
+
+		public Rectangle Tamaño
+		{
+			get
+			{
+				return Unidad.CampoBatalla.Área;
+			}
+		}
+
+
+		public abstract Vector2 VectorDeUnidad (Unidad unidad);
+
+		public abstract Vector2 VectorDeMuro ();
+
+		public virtual Vector2 VectorMovimiento ()
+		{
+			var vectorMov = new Vector2 ();
+			foreach (var u in Unidad.CampoBatalla.UnidadesVivas)
+			{
+				vectorMov += VectorDeUnidad (u);
+			}
+
+			// Sumar las paredes
+			vectorMov += VectorDeMuro ();
+
+			return vectorMov;
+		}
+
+		protected virtual void EjecutarMov (TimeSpan time)
+		{
+			var vectorMov = VectorMovimiento ();
+			Unidad.Mover (vectorMov, time);
+		}
+
+		protected virtual void OnTerminar ()
+		{
+			Unidad.OrdenActual = null;
+			AlTerminar?.Invoke ();
+		}
+
+		public abstract UpdateReturnType Update (TimeSpan time);
+
+		public event Action AlTerminar;
+	}
+}
+
