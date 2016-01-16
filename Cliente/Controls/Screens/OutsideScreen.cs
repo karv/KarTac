97ab93ca1eec06d9a50068c3c72be5bb9b5c2d3b
Edgar Carballo <karvayoEdgar@gmail.com>
2016-01-16 17:46:41 -0600

    Dibujar formas toma en cuenta el manejador de vistas

--- Cliente/Controls/Screens/OutsideScreen.cs
+++ Cliente/Controls/Screens/OutsideScreen.cs
@@ -118,7 +118,7 @@ namespace KarTac.Cliente.Controls.Screens
 		{
 			var r = Utils.Rnd;
 			//var campoBatalla = new Campo (new Point (GetDisplayMode.Width, GetDisplayMode.Height));
-			var campoBatalla = new Campo (new Point (1500, 800));
+			var campoBatalla = new Campo (new Point (5000, 1200));
 			for (int i = 0; i < 2; i++)
 			{
 				var p = new Pared (campoBatalla.Área.GetRandomPoint ().ToVector2 (),
